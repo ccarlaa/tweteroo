9ddad2e1b60deeea45312cc935764f111d27fa8e
Carla A. C. Ribeiro <carlacarlaclementino@gmail.com>
2022-04-25 17:32:06 -0300

    finished project

---
+++ app.js
@@ -0,0 +1,41 @@
+import express from "express";
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+let user = { username: "", avatar: "" };
+let tweets = [];
+
+app.post('/sign-up', (req, res) => {
+    let login = req.body;
+    user = {username: login.username, avatar: login.avatar}
+    res.status(201).send("OK")
+})
+
+app.post('/tweets', (req, res) => {
+    tweets.push({
+        username: req.body.username,
+        avatar: user.avatar,
+        tweet: req.body.tweet 
+    });
+    res.status(201).send("OK")
+})
+
+app.get('/tweets', (req, res) => {
+    let reverse = [];
+    if(tweets.length > 10) {
+        for(let i = 0; i < 9; i++) {
+            reverse.push(tweets[tweets.length -1 - i])
+        }
+    }else{
+        for(let i = 0; i < tweets.length; i++) {
+            reverse.push(tweets[tweets.length -1 - i])
+        }
+    }
+    console.log(reverse)
+    res.status(201).send(reverse)
+})
+
+app.listen(5000)
